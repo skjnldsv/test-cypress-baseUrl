db997c6c6ae079499dc357ee8a4429c771500616
John Molakvoæ <skjnldsv@protonmail.com>
2022-06-30 11:33:57 +0200

    test before:run config change
    Signed-off-by: John Molakvoæ <skjnldsv@protonmail.com>

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,18 @@
 module.exports = {
-  e2e: {
-    setupNodeEvents(on, config) {},
-  },
+	e2e: {
+		setupNodeEvents(on, config) {
+			on('before:run', () => {
+				startNextcloud().then((ip) => {
+					// Setting container's IP as base Url
+					config.baseUrl = `http://${ip}/index.php`
+					console.log(config)
+				})
+			})
+		}
+	},
+}
+
+// dummy method for testing purpose
+const startNextcloud = async function() {
+	return '172.10.11.12'
 }
